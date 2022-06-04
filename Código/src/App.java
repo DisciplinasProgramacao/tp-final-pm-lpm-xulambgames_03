0bd559b1aee8a835b7e830fbc309fd844d08d532
Vítor José <vitorjoselara@gmail.com>
2022-06-04 17:03:47 -0300

    App

--- Código/src/App.java
+++ Código/src/App.java
@@ -1,5 +1,57 @@
+import java.util.Scanner;
+
 public class App {
+
+    private static int opcao;
+    private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        
+        do {
+            System.out.println("\n                  *****      Xulamb Games 03      *****                ");
+            System.out.println("       ================================================================= ");
+            System.out.println("      |                                                                 |");
+            System.out.println("      |    1. Cadastrar cliente                                         |");
+            System.out.println("      |    2. Cadastrar jogo                                            |");
+            System.out.println("      |    3. Lançar compra                                             |");
+            System.out.println("      |    4. Histórico de um cliente específico                        |");
+            System.out.println("      |    5. Calcular o valor de uma nova compra                       |");
+            System.out.println("      |                                                                 |");
+            System.out.println("      |    0. Sair                                                      |");
+            System.out.println("      *=================================================================*\n");
+            System.out.print("Informe uma opcao: ");
+
+            opcao = sc.nextInt();
+            System.out.println("");
+
+            switch (opcao) {
+
+                case 1:
+
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+
+                    break;
+
+
+                case 0:
+                    break;
+            }
+
+        } while (opcao != 0);
     }
 }
