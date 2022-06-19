54f599bc76eea809a16879ae72bb885268647d70
Vítor José <vitorjoselara@gmail.com>
2022-06-18 21:22:41 -0300

    Validar senha e email

--- Código/src/aplicacao/App.java
+++ Código/src/aplicacao/App.java
@@ -2,8 +2,11 @@ package aplicacao;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 import cliente.Cliente;
+import cliente.ICliente;
 import compra.Compra;
 import jogo.IJogo;
 import jogo.Jogo;
@@ -37,7 +40,7 @@ public class App {
             switch (opcao) {
 
                 case 1:
-
+                    cadastrarCliente();
                     break;
 
                 case 2:
@@ -79,12 +82,83 @@ public class App {
                                 .mapToDouble(Compra :: getValorPago).sum()).average().getAsDouble();
 	}
 
-	public Jogo jogoMaisVendido() {
-		return null;
+    public Jogo jogoMaisVendido() {
+        return (Jogo) listaJogo.stream()
+                            .max((a,b) -> a.equals(b) ? 1 : -1).get();      
+
+    }
+
+    public Jogo jogoMenosVendido() {
+        return (Jogo) listaJogo.stream()
+                            .max((a,b) -> a.equals(b) ? -1 : 1).get();   
 	}
 
-	public Jogo jogoMenosVendido() {
-		return null;
-	}
+    public static void cadastrarCliente() throws Exception {
+
+        sc.nextLine();
+        System.out.println("Informe o seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Informe o seu nome de usuário: ");
+        String nomeDeUsuario = sc.nextLine();
+
+        System.out.println("Informe sua senha: ");
+        String senha = sc.nextLine();
+
+        while (!isValidPassword(senha)) {
+            System.out.println("Informe sua senha: ");
+            senha = sc.nextLine();
+        }        
+
+        System.out.println("Informe seu email: ");
+        String email = sc.nextLine();
+
+        while (!isValidEmailAddressRegex(email)) {
+            System.out.println("Informe seu email: ");
+            email = sc.nextLine();
+        }
+        
+        ICliente cliente = new Cliente(email, nomeDeUsuario, senha, cliente);
+        listaCliente.put(cliente);
+
+        
+    }
+
+    public static boolean isValidEmailAddressRegex(String email) {
+        boolean isEmailIdValid = false;
+        if (email != null && email.length() > 0) {
+            String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+            Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(email);
+            if (matcher.matches()) {
+                isEmailIdValid = true;
+            } else
+            System.out.println("\nFavor inserir um email válido!\n");
+        }
+        return isEmailIdValid;
+    }
+
+    public static boolean isValidPassword (String senha) {
+
+        System.out.println("\nFavor criar uma senha que respeita as sequintes regras:");
+        System.out.println(); 
+        System.out.println("conter entre 8 e 20 caracteres.");
+        System.out.println("Conter pelo menos um dígito.");
+        System.out.println("Conter pelo menos um alfabeto maiúsculo.");
+        System.out.println("Conter pelo menos um alfabeto minúsculo.");
+        System.out.println("Conter pelo menos um caractere especial que inclui ! @ # $% & *() - + = ^ .");
+        System.out.println("Não conter nenhum espaço em branco.\n");
+  
+        String regex = "^(?=.*[0-9])"
+                       + "(?=.*[a-z])(?=.*[A-Z])"
+                       + "(?=.*[@#$%^&+=])"
+                       + "(?=\\S+$).{8,20}$";
+        Pattern p = Pattern.compile(regex);
+        if (senha == null) {
+            return false;
+        }
+        Matcher m = p.matcher(senha);
+        return m.matches();
+    }
 }
 
